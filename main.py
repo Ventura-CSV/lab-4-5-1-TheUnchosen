b60171b0c4140e0d030d5820fb9a5ffceaa23b48
TheUnchosen <longbui369@gmail.com>
2024-06-29 19:01:50 +0000

    2222

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ def main():
     Code Your Program here
     ########################################
     """
+    numbers = []
+    i = 0
+    while i < 5:
+        numbers.append(random.randint(0,100))
+        i += 1
+    total = sum(numbers)
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
